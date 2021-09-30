0ab87c1820c702b4220bc2f7629854e31c8760a5
Thomas Erdmann <thomas@innofy.nl>
2021-09-30 23:13:29 +0200

    Cleanup code

--- creditcardchecker.js
+++ creditcardchecker.js
@@ -1,85 +1,93 @@
 // All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
 // All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
 // Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
-
+const batch = [
+	valid1,
+	valid2,
+	valid3,
+	valid4,
+	valid5,
+	invalid1,
+	invalid2,
+	invalid3,
+	invalid4,
+	invalid5,
+	mystery1,
+	mystery2,
+	mystery3,
+	mystery4,
+	mystery5,
+];
 
 // Add your functions below:
-validateCred = array => {
-    let toMultiply = true;
-    let revArr = [];
-    let newArr = [];
+validateCred = (array) => {
+	let toMultiply = true;
+	let revArr = [];
+	let newArr = [];
 
-    // Reverse array
-    for (let i = array.length + -1; i >= 0; i--) {
-        revArr.push(array[i]);
-    }
+	// Reverse array
+	for (let i = array.length + -1; i >= 0; i--) {
+		revArr.push(array[i]);
+	}
 
-    //Calculate numbers and return array
-    for (i = 1; i < array.length; i++) {
-        if (toMultiply === true) {
-            let value = revArr[i] * 2;
+	//Calculate numbers and return array
+	for (i = 1; i < array.length; i++) {
+		if (toMultiply === true) {
+			let value = revArr[i] * 2;
 
-            if (value > 9) {
-                value -= 9;
-            }
-            newArr.push(value);
-            toMultiply = false;
+			if (value > 9) {
+				value -= 9;
+			}
+			newArr.push(value);
+			toMultiply = false;
+		} else {
+			toMultiply = true;
+			newArr.push(revArr[i]);
+		}
+	}
+	newArr.unshift(revArr[0]);
+	const reducer = (accumulator, curr) => accumulator + curr;
+	calculatedSum = newArr.reduce(reducer) % 10;
 
-        } else {
-            toMultiply = true;
-            newArr.push(revArr[i]);
-        }
-    }
-    newArr.unshift(revArr[0]);
-    const reducer = (accumulator, curr) => accumulator + curr;
-    calculatedSum = newArr.reduce(reducer) % 10;
-
-    if (calculatedSum === 0) {
-        return true;
-    } else {
-        return false;
-    }
-
+	if (calculatedSum === 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
+// Check for invalid cards and return array
+const findInvalidCards = (array) => {
+	let invalidCards = [];
+	for (cc of array) {
+		if (validateCred(cc) === false) {
+			console.log('false card');
+			invalidCards.push(cc);
+			// console.log(Array.isArray(cc));
+		}
+	}
+	return invalidCards;
 };
 
-const findInvalidCards = array => {
-    let invalidCards = [];
-    for (cc of array) {
-        // validateCred(cc)
-        if (validateCred(cc) === false) {
-            console.log('false card')
-            invalidCards.push(cc);
-            console.log(invalidCards)
-        }
-    }
-    return invalidCards;
-}
-console.log(batch)
-console.log("Output = " + findInvalidCards(batch));
-
-
-
-
-
-
-
+console.log(batch);
+console.log('Output = ' + findInvalidCards(batch));
+test = findInvalidCards(batch);
+console.log(test);
