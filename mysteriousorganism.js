9bc5cbfb65d957d79b5901d93e87348be39df116
Thomas Erdmann <thomas@innofy.nl>
2021-10-01 19:01:26 +0200

    Create mutate method and test variables

--- mysteriousorganism.js
+++ mysteriousorganism.js
@@ -18,8 +18,26 @@ const pAequorFactory = (specimenNum, dna) => {
 	return {
 		specimenNum,
 		dna,
+
+		mutate() {
+			const dnaBases = ['A', 'T', 'C', 'G']; // You'd probably want this variable to be global at some point.
+			const randomBase = Math.floor(Math.random() * 15);
+			const replaceBase = dna[randomBase];
+			let newBase = '';
+
+			// Find value at random place in base and replace it with a different base
+			do {
+				newBase = dnaBases[Math.floor(Math.random() * 3)];
+			} while (replaceBase === newBase);
+			dna.splice(randomBase, 1, newBase);
+			return dna;
+		},
 	};
 };
 
 //Test functionfactory
-console.log(pAequorFactory('Specimen', 'DNA'));
+testNumber = 1;
+testStrand = ['G', 'C', 'G', 'T', 'C', 'T', 'C', 'G', 'T', 'C', 'A', 'A', 'A', 'T', 'A'];
+testObject = pAequorFactory(testNumber, testStrand);
+console.log(testObject.dna);
+console.log(testObject.mutate());
