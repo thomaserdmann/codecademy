9914f58e495af1b8da63e6ef01cb8f509edfc73c
Thomas Erdmann <thomas@innofy.nl>
2021-10-02 13:44:09 +0200

    Add errorhandling findBestMatch

--- mysteriousorganism.js
+++ mysteriousorganism.js
@@ -95,19 +95,26 @@ const findBestMatch = testDatabase => {
 	let match1 = 0;
 	let match2 = 0;
 	let highestMatch = 0;
+	let matchCount = 0;
 
 	for (strand of testDatabase) {
 		for (j = testDatabase.indexOf(strand) + 1; j < testDatabase.length; j++) {
 			let match = strand.compareDNA(testDatabase[j]);
 
+			if (match == highestMatch) {
+				matchCount += 1;
+			}
 			if (match > highestMatch) {
 				match1 = strand.specimenNum;
 				match2 = testDatabase[j].specimenNum;
 				highestMatch = match;
+				matchCount = 1;
 			}
 		}
 	}
-	return `Element ${match1} and element ${match2} have the highest match of ${highestMatch}%`;
+	if (matchCount == 1) {
+		return `Element ${match1} and element ${match2} have the highest match of ${highestMatch}%`;
+	} else return `Error: ${matchCount} elements with highest match of ${highestMatch}%`;
 };
 
 //Test functionfactory
@@ -123,9 +130,8 @@ testNumber3 = 3;
 testStrand3 = ['G', 'G', 'G', 'A', 'A', 'C', 'A', 'T', 'T', 'C', 'C', 'G', 'G', 'A', 'A'];
 testObject3 = pAequorFactory(testNumber3, testStrand3);
 
-testDatabase = createDatabase(5);
-console.log(findBestMatch(testDatabase));
-console.log(testDatabase);
+testDatabase = createDatabase(500);
+// console.log(testDatabase);
 console.log(testDatabase[0].compareDNA(testDatabase[1]));
 console.log(testDatabase[0].compareDNA(testDatabase[2]));
 console.log(testDatabase[0].compareDNA(testDatabase[3]));
@@ -136,6 +142,8 @@ console.log(testDatabase[1].compareDNA(testDatabase[4]));
 console.log(testDatabase[2].compareDNA(testDatabase[3]));
 console.log(testDatabase[2].compareDNA(testDatabase[4]));
 console.log(testDatabase[3].compareDNA(testDatabase[4]));
+
+console.log(findBestMatch(testDatabase));
 
 // console.log(testDatabase[3].compareDNA(testDatabase[0])); //
 // console.log(testDatabase[3].compareDNA(testDatabase[1])); //
