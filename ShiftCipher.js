5bce63b1fb8fcef8b58585c41f9ac587eae0a162
Thomas Erdmann <thomas@innofy.nl>
2022-03-24 20:41:18 +0100

    Add challenge description

--- ShiftCipher.js
+++ ShiftCipher.js
@@ -1,23 +1,19 @@
-// const test = 'I love to code!';
-// const testUppercase = test.toUpperCase();
-// let cipherArray = [];
-// const shift = 2;
+// A shift cipher takes a plain text message and shifts each letter forward in the alphabet by a given number. For example, a shift cipher with a shift of 1 would turn the string 'hello' to 'ifmmp'.
 
-// for (let i = 0; i < testUppercase.length; i++) {
-// 	j = testUppercase.charCodeAt(i);
+// Create a class ShiftCipher that takes the numerical value of the shift as a constructor parameter. The class should have two methods:
 
-// 	if (j >= 65 && j <= 90) {
-// 		shiftResult = j + shift;
-// 		if (shiftResult >= 90) {
-// 			shiftResult = 64 + shift;
-// 		}
-// 		cipherArray.push(String.fromCharCode(shiftResult));
-// 	} else {
-// 		cipherArray.push(String.fromCharCode(j));
-// 	}
-// }
+// encrypt: takes a plain text string and returns a capitalized string with each letter shifted forward in the alphabet based on the set shift value.
+// decrypt: takes an encrypted message and returns a lower case string with each letter shifted back in the alphabet based on the set shift value.
+// In both methods, any character outside the alphabet should remain the same.
+// But if a character is shifted outside the alphabet in either direction it should be wrapped around to the other side. For example, encrypting a y with a shift of 4 results in C and decrypting an A with a shift of 1 result in z.
+// Example:
 
-// console.log(cipherArray.join(''));
+// const cipher = new ShiftCipher(2);
+// cipher.encrypt('I love to code!'); // returns 'K NQXG VQ EQFG!'
+// cipher.decrypt('K <3 OA RWRRA'); // returns 'i <3 my puppy'
+// Feel free to reference the Unicode Table as well as the JavaScript String methods including: toUpperCase(), toLowerCase(), charCodeAt() and fromCharCode()
+
+
 
 class ShiftCipher {
 	constructor(shift) {
@@ -52,5 +48,4 @@ class ShiftCipher {
 }
 
 const cipher = new ShiftCipher(2);
-
 console.log(cipher.encrypt('I love to code!'));
