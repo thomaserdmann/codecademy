c335747fbe5bfefa67df8f4854bec8d75e27b2af
Thomas Erdmann <thomas@innofy.nl>
2022-03-27 15:30:22 +0200

    Add test script

---
+++ vendingmachine/test/vendingmachine-test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const vm = require('../vendingmachine');
+const VendingMachine = require('../vendingmachine');
+
+describe('vm', () => {
+	describe('.getProduct', () => {
+		it('returns productname when entering number', () => {
+			//Setup
+			const productNumber = 6;
+			const expected = 'Condoms';
+
+			//Exercise
+			const result = vm.getProduct(6).name;
+
+			//Verify
+			assert.strictEqual(expected, result);
+		});
+	});
+});
