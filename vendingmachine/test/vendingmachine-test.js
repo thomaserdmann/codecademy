01e029f0f7479c6de657e6a3bf7d6a884efd7c97
Thomas Erdmann <thomas@innofy.nl>
2022-03-27 15:52:37 +0200

    Add testing requirements

--- vendingmachine/test/vendingmachine-test.js
+++ vendingmachine/test/vendingmachine-test.js
@@ -28,4 +28,58 @@ describe('vm', () => {
 			assert.strictEqual(expected, result);
 		});
 	});
+
+	describe('.placeOrder', () => {
+		it('returns an object with 2 properties, product and change', () => {
+			//Setup
+			const productNumber = 6;
+			const money = 600;
+			const expected = { product: 'Condoms', change: [100] };
+
+			//Exercise
+			result = vm.placeOrder(productNumber, money);
+
+			//Verify
+			assert.deepEqual(expected, result);
+		});
+
+		it('returns a string when productNumber is invalid', () => {
+			//Setup
+			const productNumber = 200;
+			const money = 600;
+			const expected = 'Enter a valid product number';
+
+			//Exercise
+			result = vm.placeOrder(productNumber, money);
+
+			//Verify
+			assert.strictEqual(expected, result);
+		});
+
+		it('returns a string when not enough money', () => {
+			//Setup
+			const productNumber = 6;
+			const money = 5;
+			const expected = 'Not enough money for this product';
+
+			//Exercise
+			result = vm.placeOrder(productNumber, money);
+
+			//Verify
+			assert.strictEqual(expected, result);
+		});
+
+		it('returns an empty array when no change', () => {
+			//Setup
+			const productNumber = 6;
+			const money = 500;
+			const expected = [];
+
+			//Exercise
+			result = vm.placeOrder(productNumber, money).change;
+
+			//Verify
+			assert.deepEqual(expected, result);
+		});
+	});
 });
