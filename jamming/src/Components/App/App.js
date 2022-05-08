ef5c614a205fbd488811f412598569d92b3d53a7
Thomas Erdmann <thomas@innofy.nl>
2022-05-08 19:29:11 +0200

    Implement removeTrack method

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -22,16 +22,24 @@ class App extends React.Component {
 			],
 		};
 
-    this.addTrack = this.addTrack.bind(this);
-
+		this.addTrack = this.addTrack.bind(this);
+		this.removeTrack = this.removeTrack.bind(this);
 	}
 
 	addTrack(track) {
-		if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+		let tracks = this.state.playlistTracks;
+		if (tracks.find(savedTrack => savedTrack.id === track.id)) {
 			return;
 		}
-		this.state.playlistTracks.push(track);
-    this.setState({playlistTrack: this.state.playlistTracks})
+		tracks.push(track);
+		this.setState({ playlistTracks: tracks });
+	}
+
+	removeTrack(track) {
+		let tracks = this.state.playlistTracks;
+
+		tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+		this.setState({ playlistTracks: tracks });
 	}
 
 	render() {
@@ -43,8 +51,12 @@ class App extends React.Component {
 				<div className="App">
 					<SearchBar />
 					<div className="App-playlist">
-						<SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-						<Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+						<SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+						<Playlist
+							playlistName={this.state.playlistName}
+							playlistTracks={this.state.playlistTracks}
+							onRemove={this.removeTrack}
+						/>
 					</div>
 				</div>
 			</div>
--- jamming/src/Components/TrackList/TrackList.js
+++ jamming/src/Components/TrackList/TrackList.js
@@ -7,7 +7,15 @@ class TrackList extends React.Component {
 		return (
 			<div className="TrackList">
 				{this.props.tracks.map(track => {
-					return <Track track={track} key={track.id} onAdd={this.props.onAdd} />;
+					return (
+						<Track
+							track={track}
+							key={track.id}
+							onAdd={this.props.onAdd}
+							onRemove={this.props.onRemove}
+							isRemoval={this.props.isRemoval}
+						/>
+					);
 				})}
 			</div>
 		);
