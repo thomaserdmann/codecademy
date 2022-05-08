cf6263171fdf97e357bb2e96885ca8d46450e007
Thomas Erdmann <thomas@innofy.nl>
2022-05-08 19:43:48 +0200

    Add changeName method

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
 
 		this.addTrack = this.addTrack.bind(this);
 		this.removeTrack = this.removeTrack.bind(this);
+		this.updatePlaylistName = this.updatePlaylistName.bind(this);
 	}
 
 	addTrack(track) {
@@ -42,6 +43,10 @@ class App extends React.Component {
 		this.setState({ playlistTracks: tracks });
 	}
 
+	updatePlaylistName(name) {
+		this.setState({ playlistName: name });
+	}
+
 	render() {
 		return (
 			<div>
@@ -56,6 +61,7 @@ class App extends React.Component {
 							playlistName={this.state.playlistName}
 							playlistTracks={this.state.playlistTracks}
 							onRemove={this.removeTrack}
+							onNameChange={this.updatePlaylistName}
 						/>
 					</div>
 				</div>
