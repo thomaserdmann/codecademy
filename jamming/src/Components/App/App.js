01540939688eb758c55a8668d8859090d2426ad9
Thomas Erdmann <thomas@innofy.nl>
2022-05-08 14:27:23 +0200

    Add components

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			searchResults: [
+				{ name: 'Name 1', artist: 'Artist 1', album: 'Album 1', id: 1 },
+				{ name: 'Name 2', artist: 'Artist 2', album: 'Album 2', id: 2 },
+				{ name: 'Name 3', artist: 'Artist 3', album: 'Album 3', id: 3 },
+			],
+		};
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>
+				<h3>
 					Ja<span className="highlight">mmm</span>ing
-				</h1>
+				</h3>
 				<div className="App">
-					{/* <Searchbar /> */}
+					<SearchBar />
 					<div className="App-playlist">
-						{/* <SearchResults /> */}
-						{/* <Playlist /> */}
+						<SearchResults searchResults={this.state.searchResults} />
+						<Playlist />
 					</div>
 				</div>
 			</div>
---
+++ jamming/src/Components/Track/Track.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+	renderAction() {
+		if (this.props.isRemoval) {
+			return <button className="Track-action">-</button>;
+		} else {
+			return <button className="Track-action">+</button>;
+		}
+	}
+
+	render() {
+		return (
+			<div className="Track">
+				<div className="Track-information">
+					<h3>{this.props.track.name}</h3>
+					<p>
+						{this.props.track.artist} | {this.props.track.album}
+					</p>
+				</div>
+				{this.renderAction()}
+			</div>
+		);
+	}
+}
+
+export default Track;
