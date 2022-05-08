13fb44d54c8515a07e4fa6718e8edcc6ced4af9b
Thomas Erdmann <thomas@innofy.nl>
2022-05-08 09:24:04 +0200

    Add base styling

---
+++ jamming/src/Components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Ja<span className="highlight">mmm</span>ing
+				</h1>
+				<div className="App">
+					{/* <Searchbar /> */}
+					<div className="App-playlist">
+						{/* <SearchResults /> */}
+						{/* <Playlist /> */}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
